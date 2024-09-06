30a40e579cbb93f8742cd227d2d7df902d404bec
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2024-09-06 22:48:21 +0530

    authapp

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+
+const { login, signup } = require("../controller/Auth");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
+
+router.post("/login", login);
+router.post("/signup", signup);
+router.get("/test", auth, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for auth",
+  });
+});
+
+// protected route
+router.get("/student", auth, isStudent, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for Students",
+  });
+});
+router.get("/admin", auth, isAdmin, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for isAdmin",
+  });
+});
+
+module.exports = router;
